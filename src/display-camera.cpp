6a8a24abffdd83cdb7e1a5a0c4a0e71a98ee47db
erget <lee@isi-solutions.org>
2013-12-17 11:01:02 +0000

    Modify display-camera to show video or stills

--- src/display-camera.cpp
+++ src/display-camera.cpp
@@ -11,10 +11,37 @@
 #include "RaspiCamCV.h"
 
 int main(int argc, char** argv) {
-    RaspiCamCvCapture * camera = raspiCamCvCreateCameraCapture(0);
-    cv::Mat image(raspiCamCvQueryFrame(camera));
-    std::string window_name = "Display";
-    cv::namedWindow(window_name, CV_WINDOW_AUTOSIZE);
-    cv::imshow(window_name, image);
-    cv::waitKey(0);
+    int ret_value = 0;
+    const std::string binary_name(argv[0]);
+    const std::string USAGE =
+        "Usage: " + binary_name + " [still/video]\n";
+    bool valid;
+    std::string run_type;
+    if (argc == 2) {
+        run_type= argv[1];
+        valid = run_type == "still" || run_type == "video";
+    }
+    else valid = false;
+
+    if (valid) {
+        RaspiCamCvCapture * camera = raspiCamCvCreateCameraCapture(0);
+        cv::Mat image(raspiCamCvQueryFrame(camera));
+        std::string window_name = "Display";
+        cv::namedWindow(window_name, CV_WINDOW_AUTOSIZE);
+        if (run_type == "still") {
+            cv::imshow(window_name, image);
+            cv::waitKey(0);
+        } else {
+            int pressed_key = 0;
+            do {
+                cv::Mat image(raspiCamCvQueryFrame(camera));
+                cv::imshow(window_name, image);
+                pressed_key = cv::waitKey(1);
+            } while (pressed_key != 27);
+        }
+    } else {
+        std::cout << USAGE;
+        ret_value = 1;
+    }
+    return ret_value;
 }
