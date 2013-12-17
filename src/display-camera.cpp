884985dfa4f97021182c5a815ca5a8fda0cef282
erget <lee@isi-solutions.org>
2013-12-17 12:56:24 +0000

    Allow user to specify capture dimensions

--- src/display-camera.cpp
+++ src/display-camera.cpp
@@ -3,6 +3,7 @@
  */
 
 #include <iostream>
+#include <sstream>
 #include <string>
 
 #include <opencv2/core/core.hpp>
@@ -14,17 +15,36 @@ int main(int argc, char** argv) {
     int ret_value = 0;
     const std::string binary_name(argv[0]);
     const std::string USAGE =
-        "Usage: " + binary_name + " [still/video]\n";
+        "Usage: " + binary_name + " [still/video] [width] [height]\n"
+        "\n"
+        "Width and height determine the dimensions of the picture.\n"
+        "Width and height should be proportioned 4:3. Possible values:\n"
+        "    640 480 (small picture)\n"
+        "    1280 960 (larger picture)\n"
+        "    2592 1944 (camera's native resolution)\n"
+        "If no value is provided, width and height default to 640 480.\n"
+        ;
     bool valid;
     std::string run_type;
-    if (argc == 2) {
+    int width = 640;
+    int height = 480;
+    if (argc == 2 || argc == 4) {
         run_type= argv[1];
         valid = run_type == "still" || run_type == "video";
+        if (argc == 4) {
+            std::stringstream string_width(argv[2]);
+            std::stringstream string_height(argv[3]);
+            string_width >> width;
+            string_height >> height;
+            if (string_width.fail() || string_height.fail())
+                valid = false;
+        }
     }
     else valid = false;
 
     if (valid) {
-        RaspiCamCvCapture * camera = raspiCamCvCreateCameraCapture(0);
+        RaspiCamCvCapture * camera =
+            raspiCamCvCreateCameraCaptureWithDims(width, height);
         cv::Mat image(raspiCamCvQueryFrame(camera));
         std::string window_name = "Display";
         cv::namedWindow(window_name, CV_WINDOW_AUTOSIZE);
